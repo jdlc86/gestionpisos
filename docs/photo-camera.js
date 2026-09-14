89baba9d05b38b97b056a4dc7804d4d06ab87a4a
jdlc86 <julopezcardona@gmail.com>
2026-09-15 00:30:44 +0200

    chore: version manual alignment scoring

--- docs/photo-camera.js
+++ docs/photo-camera.js
@@ -148,5 +148,5 @@
 
 const cameraMode = new URLSearchParams(window.location.search).get("mode");
 if (cameraMode !== "pattern") {
-  import("./photo-alignment.js?v=2026091502").catch(()=>{});
+  import("./photo-alignment.js?v=2026091503").catch(()=>{});
 }

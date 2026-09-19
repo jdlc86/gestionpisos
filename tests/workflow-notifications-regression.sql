3620edb0339b1abc70c2b6e1f4c4e6337297c99d
jdlc86 <julopezcardona@gmail.com>
2026-09-19 19:25:21 +0200

    Use independent admin in notification regression

--- tests/workflow-notifications-regression.sql
+++ tests/workflow-notifications-regression.sql
@@ -11,7 +11,7 @@ insert into public.user_roles(user_id,organization_id,role)
 values (
   '33333333-3333-4333-8333-333333333333',
   '11111111-1111-4111-8111-111111111111',
-  'employee'
+  'admin'
 )
 on conflict do nothing;
 

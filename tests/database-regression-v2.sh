b138d72342e82aa573bab3487552a29006271e1c
jdlc86 <julopezcardona@gmail.com>
2026-09-22 08:56:09 +0200

    test(workflows): run final E2E preflight in schema regression

--- tests/database-regression-v2.sh
+++ tests/database-regression-v2.sh
@@ -215,6 +215,7 @@ docker run --rm   -e POSTGRES_PASSWORD=local-regression-only   -e WF04_FOCUSED="
     psql -v ON_ERROR_STOP=1 -U postgres -f /work/tests/workflow-wf06-domain-regression.sql
     psql -v ON_ERROR_STOP=1 -U postgres -f /work/tests/workflow-wf07-domain-regression.sql
     psql -v ON_ERROR_STOP=1 -U postgres -f /work/tests/workflow-legacy-closure-regression.sql
+    psql -v ON_ERROR_STOP=1 -U postgres -f /work/tests/workflow-final-e2e-preflight.sql
 
     trap - EXIT
     cleanup
